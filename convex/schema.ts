f1f9ec4430d45c7690adc463e33b069169df2d91
Alec Li <smartspot2@gmail.com>
2023-10-28 10:16:58 -0700

    Delete old app.ts, add db schema

---
+++ convex/schema.ts
@@ -0,0 +1,21 @@
+import { defineSchema, defineTable } from 'convex/server';
+import { v } from 'convex/values';
+
+export default defineSchema({
+  // table of current games
+  games: defineTable({
+    users: v.array(v.string()),
+  }),
+  // table of current grid states
+  grids: defineTable({
+    // user associated with the grid
+    user: v.string(),
+    // grid state
+    state: v.object({
+      // mine reference numbers (9 for mine)
+      numbers: v.array(v.array(v.number())),
+      // whether the user has clicked, flagged, or not touched the cell
+      covers: v.array(v.array(v.number())),
+    }),
+  }),
+});
