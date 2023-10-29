ba1fd9973e2666dfc38d13c0d5ace31efe218bbc
jasonz <jazhang00@gmail.com>
2023-10-28 20:29:44 -0700

    Integrate with backend: add loser, add winner, send and receive disruptions

--- src/components/minesweeper/Game.tsx
+++ src/components/minesweeper/Game.tsx
@@ -5,9 +5,12 @@ import './Game.css';
 import { useMutation, useQuery } from 'convex/react';
 import { api } from '../../../convex/_generated/api';
 import { useState } from 'react';
-
+import { disruptBoard } from './util/disrupt';
+import { Doc } from '../../../convex/_generated/dataModel';
 
 interface GameProps {
+  username: string;
+  game: Doc<'games'>;
   initialValues: number[][];
   initialCovers: number[][];
 }
@@ -15,22 +18,65 @@ interface GameProps {
 /**
  * Main component for the minesweeper game.
  */
-export const Game = ({initialValues: initial_values, initialCovers: initial_covers}: GameProps) => {
+export const Game = ({username, game, initialValues: initial_values, initialCovers: initial_covers}: GameProps) => {
   const [values, setValues] = useState(initial_values);
   const [covers, setCovers] = useState(initial_covers);
+  const [mined, setMined] = useState(initial_covers.map((row) => Array(row.length).fill(false)));
+
+  const [death, setDeath] = useState(false);
+  const [win, setWin] = useState(false);
+
+  const resolveDisruption = useMutation(api.mutations.game.resolveDisruption);
+  const addLoser = useMutation(api.mutations.game.addLoser);
+  const addWinner = useMutation(api.mutations.game.addWinner);
+  const disruptUser = useMutation(api.mutations.game.disruptUser);
+
+  let num_disruptions = 0;
+  for (let i = 0; i < game.users.length; i++) {
+    if (game.users[i].username === username) {
+      num_disruptions = game.users[i].disruptions;
+    }
+  }
+
+  if (num_disruptions > 0) {
+    let new_values = values.map((row) => [...row]);
+    let new_covers = covers.map((row) => [...row]);
+    disruptBoard(new_values, new_covers);
+
+    setValues(new_values);
+    setCovers(new_covers);
+    resolveDisruption({
+      game_id: game._id, 
+      username: username, 
+      new_disruption_count: num_disruptions - 1
+    });
+  }
+
+  if (!win) {
+    const checkedWin = checkWin(values, covers);
+    if (checkedWin) {
+      addWinner({game_id: game._id, username: username});
+      setWin(true);
+    }
+  }
 
   const onClick = (row: number, col: number) => {
+    if (death || win) {
+      return;
+    }
+
     let new_covers = covers.map((row) => [...row]);
 
     const cover = covers[row][col];
     if (cover === 0) {
-      // todo -- middle mouse click
+      // TODO -- middle mouse click
       return;
     } else if (cover === 1) {
       expand(values, new_covers, row, col);
       if (values[row][col] === 9) {
         // hit mine -- death
-
+        setDeath(true);
+        addLoser({game_id: game._id, username: username});
       }
     } else if (cover === 2) {
       // flag -- no change
@@ -41,18 +87,28 @@ export const Game = ({initialValues: initial_values, initialCovers: initial_cove
   };
 
   const onFlag = (row: number, col: number) => {
+    if (death || win) {
+      return;
+    }
+
     const cover = covers[row][col];
     let new_covers = covers.map((row) => [...row]);
     if (cover === 1) {
       new_covers[row][col] = 2;
+      
+      // send disruption upon finding mine in new spot
+      if (!mined[row][col] && values[row][col] == 9) {
+        let new_mined = mined.map((row) => [...row]);
+        new_mined[row][col] = true;
+
+        disruptUser({game_id: game._id, username: username});
+
+        setMined(new_mined);
+      }
     } else if (cover === 2) {
       new_covers[row][col] = 1;
     }
     setCovers(new_covers);
-  };
-
-  const onDisrupt = () => {
-
   };
 
   return (
@@ -95,3 +151,16 @@ const expand = (values: number[][], covers: number[][], row: number, col: number
     }
   }
 };
+
+
+const checkWin = (values: number[][], covers: number[][]) : boolean => {
+  let checkedWin = true;
+  for (let i = 0; i < values.length; i++) {
+    for (let j = 0; j < values[i].length; j++) {
+      if (covers[i][j] !== 0 && values[i][j] !== 9) {
+        checkedWin = false;
+      }
+    }
+  }
+  return checkedWin;
+}
